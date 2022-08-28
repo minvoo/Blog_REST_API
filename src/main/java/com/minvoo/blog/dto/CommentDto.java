cc86da9c3a44b282748996d01cda547ca9cba25d
Mariusz <maciejewski_mariusz@wp.pl>
2022-08-28 17:45:44 +0200

    validation

--- src/main/java/com/minvoo/blog/dto/CommentDto.java
+++ src/main/java/com/minvoo/blog/dto/CommentDto.java
@@ -6,10 +6,22 @@ import lombok.experimental.Accessors;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
     private long id;
+
+    @NotEmpty(message = "Name should not be empty")
     private String name;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email
     private String email;
+
+    @NotEmpty(message = "Body should not be empty")
+    @Size(min=10, message = "Body should have at least 10 characters")
     private String body;
 }
